10003909686808b02bdd475f0c00d38d9d3bc6fa
lucaramiandrasoa <ramiandrasoaluca@gmail.com>
2023-02-23 00:45:57 +0100

    first commit

---
+++ projet_simple/src/main/java/com/mycompany/projet_simple/Projet_simple.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_simple;
+
+/**
+ *
+ * @author LukasRAMIANDRASOA
+ */
+public class Projet_simple {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
