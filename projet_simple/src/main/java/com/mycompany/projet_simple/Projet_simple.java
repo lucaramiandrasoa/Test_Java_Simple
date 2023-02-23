8e306dc2873a4ce66f859f65d06709732fe967ad
lucaramiandrasoa <ramiandrasoaluca@gmail.com>
2023-02-23 01:05:05 +0100

    rajout de fonctionnalite

--- projet_simple/src/main/java/com/mycompany/projet_simple/Projet_simple.java
+++ projet_simple/src/main/java/com/mycompany/projet_simple/Projet_simple.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.projet_simple;
 
+import java.util.Scanner;
+
 /**
  *
  * @author LukasRAMIANDRASOA
@@ -11,6 +13,26 @@ package com.mycompany.projet_simple;
 public class Projet_simple {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        int nombre;
+        System.out.println("Entrer un nombre");
+        nombre = scan.nextInt();
+        
+        //Si nombre divisible par 3 alors "Fluzz"
+        //Si nombre divisible par 5 alors "Gratz"
+        //Si nombre divisible par 3 et 5 alors "FluzzGratz"
+        //sinon on affiche le nombre
+        
+        if(nombre % 3 == 0){
+            System.out.println("Fluzz");
+        }
+        else if(nombre % 5 == 0){
+            System.out.println("Gratz");
+        }
+        else if((nombre % 3 == 0) && (nombre % 5 == 0)){
+            System.out.println("FluzzGratz");
+        }else{
+            System.out.println("Le nombre est :"+nombre);
+        }
     }
 }
